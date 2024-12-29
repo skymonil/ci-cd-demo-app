fbafaa28a12d5ae637eef9ad9de9b09856fd17ac
skymonil <monilparikh11@gmail.com>
2024-12-29 14:41:53 +0530

    7th commit

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let path = require('path');
 let fs = require('fs');
+let os = require('os');
 let MongoClient = require('mongodb').MongoClient;
 let bodyParser = require('body-parser');
 let app = express();
@@ -10,28 +11,29 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// Serve the index.html
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
+// Serve profile picture
 app.get('/profile-picture', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-// use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@mongodb:27017";
-
-// use when starting application as docker container
+// MongoDB connection URLs
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 let mongoUrlDocker = "mongodb://admin:password@mongodb";
 
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
+// MongoDB connection options
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
-// "user-account" in demo with docker. "my-db" in demo with docker-compose
+// Database name
 let databaseName = "my-db";
 
+// Endpoint to update profile
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
@@ -54,9 +56,9 @@ app.post('/update-profile', function (req, res) {
   res.send(userObj);
 });
 
+// Endpoint to get profile
 app.get('/get-profile', function (req, res) {
   let response = {};
-  // Connect to the db
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
@@ -75,6 +77,25 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
+// Endpoint to get the VM IP
+app.get('/get-vm-ip', function (req, res) {
+  const interfaces = os.networkInterfaces();
+  let internalIp;
+
+  for (const iface of Object.values(interfaces)) {
+    for (const config of iface) {
+      if (config.family === 'IPv4' && !config.internal) {
+        internalIp = config.address; // Fetch the VM's internal IP
+        break;
+      }
+    }
+    if (internalIp) break;
+  }
+
+  res.json({ ip: internalIp });
+});
+
+// Start the server
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
